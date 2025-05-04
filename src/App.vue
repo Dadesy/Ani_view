98c23b6fa730a352932d13bb83c8cd12fcd8a805
konstantin <k.podkovyrov@efko.ru>
2025-05-04 22:05:35 +0300

    Добавлен модуль аналитики vercel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <a-config-provider :theme="theme">
+    <Analytics />
     <router-view />
   </a-config-provider>
 </template>
 
 <script setup lang="ts">
   import { theme } from '@/theme.js';
+  import { Analytics } from '@vercel/analytics/vue';
 </script>
--- src/App.vue.js
+++ src/App.vue.js
@@ -1,4 +1,5 @@
 import { theme } from '@/theme.js';
+import { Analytics } from '@vercel/analytics/vue';
 debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
 const __VLS_ctx = {};
 let __VLS_components;
@@ -14,17 +15,23 @@ const __VLS_2 = __VLS_1({
 }, ...__VLS_functionalComponentArgsRest(__VLS_1));
 var __VLS_4 = {};
 __VLS_3.slots.default;
-const __VLS_5 = {}.RouterView;
-/** @type {[typeof __VLS_components.RouterView, typeof __VLS_components.routerView, ]} */ ;
+const __VLS_5 = {}.Analytics;
+/** @type {[typeof __VLS_components.Analytics, ]} */ ;
 // @ts-ignore
 const __VLS_6 = __VLS_asFunctionalComponent(__VLS_5, new __VLS_5({}));
 const __VLS_7 = __VLS_6({}, ...__VLS_functionalComponentArgsRest(__VLS_6));
+const __VLS_9 = {}.RouterView;
+/** @type {[typeof __VLS_components.RouterView, typeof __VLS_components.routerView, ]} */ ;
+// @ts-ignore
+const __VLS_10 = __VLS_asFunctionalComponent(__VLS_9, new __VLS_9({}));
+const __VLS_11 = __VLS_10({}, ...__VLS_functionalComponentArgsRest(__VLS_10));
 var __VLS_3;
 var __VLS_dollars;
 const __VLS_self = (await import('vue')).defineComponent({
     setup() {
         return {
             theme: theme,
+            Analytics: Analytics,
         };
     },
 });
